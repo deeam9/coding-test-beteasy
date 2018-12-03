6223a38e98a90d6e798ed67329dc4f4cdf89cac8
Dushyant Maheshwari <dushyant.m@gmail.com>
2018-12-03 22:46:57 +1100

    jsonService returns a list of horses with their prices

--- DataProvider/Service/JsonService.cs
+++ DataProvider/Service/JsonService.cs
@@ -1,14 +1,43 @@
-﻿using DataProvider.Model;
-using DataProvider.ServiceAbstraction;
+﻿
 using System.Collections.Generic;
+using Microsoft.Extensions.Options;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using DataProvider.ServiceAbstraction;
+using DataProvider;
+using DataProvider.Model;
 
 namespace DataProvider.Service
 {
     public class JsonService : IJsonService
     {
+        private readonly string _jsonDataFilePath;
+        public JsonService(IOptions<AppSettings> appsettings)
+        {
+            _jsonDataFilePath = appsettings.Value.JsonDataFile;
+        }
+
         public List<Horse> GetHorsesFromJsonService()
         {
-            throw new System.NotImplementedException();
+            List<Horse> horseList = new List<Horse>();
+            var jsonData = JObject.Parse(File.ReadAllText(_jsonDataFilePath));
+
+            var markets = jsonData["RawData"]["Markets"];
+
+            var selections = markets[0]["Selections"];
+
+            foreach (var selection in selections)
+            {
+                var horse = new Horse
+                {
+                    Price = (double)selection["Price"],
+                    Name = selection["Tags"]["name"].ToString()
+                };
+                horseList.Add(horse);
+
+            }
+            return horseList;
         }
     }
+
 }
--- dotnet-code-challenge/Program.cs
+++ dotnet-code-challenge/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using DataProvider;
+using DataProvider.Model;
+using DataProvider.Service;
+using DataProvider.ServiceAbstraction;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace dotnet_code_challenge
 {
@@ -6,7 +14,41 @@ namespace dotnet_code_challenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GetHorsesInPriceAscOrder();
+        }
+
+        public static IConfigurationRoot Configuration { get; set; }
+        public IServiceProvider ServiceProvider { get; set; }
+
+        private static void GetHorsesInPriceAscOrder()
+        {
+            // set up configration
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+            Configuration = builder.Build();
+
+            // set up services
+            var services = new ServiceCollection();
+
+            services
+                .AddScoped<IXmlService, XmlService>()
+                .AddScoped<IJsonService, JsonService>();
+            services.AddOptions();
+
+            // set up configuration
+            services.Configure<AppSettings>(Configuration.GetSection("BetEasy"));
+
+            // Build ServiceProvider
+            var serviceProvider = services.BuildServiceProvider();
+
+            // get jsonService from serviceProvider
+            var jsonService = serviceProvider.GetService<IJsonService>();
+
+            // fetch all the horses from jsonService
+            List<Horse> horses = jsonService.GetHorsesFromJsonService();
         }
     }
 }
